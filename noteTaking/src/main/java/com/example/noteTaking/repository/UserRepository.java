c47b96d8eb1b2a5655e13cef95c06b565590c9ae
miko859 <155954510+miko859@users.noreply.github.com>
2025-06-18 11:56:35 +0200

    Delete noteTaking directory

--- noteTaking/src/main/java/com/example/noteTaking/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.noteTaking.repository;
-
-import com.example.noteTaking.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
-
